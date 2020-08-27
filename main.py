a7d1efebc2b16679c5e8a89067c363b0c7911a99
teenaanicattumathew <anicattu7@gmail.com>
2020-08-27 12:24:46 -0500

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from models.settings import db
 from models.user import User
 import hashlib
 from models.topic import Topic
+import os
+import smartninja_redis
+
+redis = smartninja_redis.from_url(os.environ.get("REDIS_URL"))
 
 app = Flask(__name__)
 db.create_all()
@@ -13,7 +17,9 @@ db.create_all()
 def index():
     session_token = request.cookies.get("session_token")
     user = db.query(User).filter_by(session_token=session_token).first()
-    return render_template("index.html", user=user)
+    topics = db.query(Topic).all()
+
+    return render_template("index.html", user=user, topics=topics)
 
 
 @app.route('/signup', methods=["GET", "POST"])
@@ -66,7 +72,7 @@ def login():
                 return "Your password is incorrect."
 
 
-@app.route('/create_topic', methods=["GET", "POST"])
+"""@app.route('/create_topic', methods=["GET", "POST"])
 def topic_create():
     if request.method == "GET":
         return render_template("topic_create.html")
@@ -83,7 +89,105 @@ def topic_create():
 
     topic = Topic.create(title=title, text=text, author=user)
 
-    return redirect(url_for('index'))
+    return redirect(url_for('index'))"""
+
+
+@app.route("/create_topic", methods=["GET", "POST"])
+def topic_create():
+    # get current user (author)
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token).first()
+
+    # only logged in users can create a topic
+    if not user:
+        return redirect(url_for('login'))
+
+    # GET method
+    if request.method == "GET":
+        csrf_token = str(uuid.uuid4())  # create CSRF token
+
+        redis.set(name=csrf_token, value=user.username)  # store CSRF token into Redis for that specific user
+        return render_template("topic_create.html", user=user, csrf_token=csrf_token)
+
+    # POST method
+    elif request.method == "POST":
+        csrf = request.form.get("csrf")  # csrf from HTML
+        redis_csrf_username = redis.get(name=csrf).decode()  # username value stored under the csrf name from redis
+
+        if redis_csrf_username and redis_csrf_username == user.username:  # if they match, allow user to create a topic
+            title = request.form.get("title")
+            text = request.form.get("text")
+
+            # create a Topic object
+            topic = Topic.create(title=title, text=text, author=user)
+
+            return redirect(url_for('index'))
+        else:
+            return "CSRF token is not valid!"
+
+
+@app.route("/topic/<topic_id>", methods=["GET"])
+def topic_details(topic_id):
+    topic = db.query(Topic).get(int(topic_id))
+    # get current user (author)
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token).first()
+
+    return render_template("topic_details.html", topic=topic, user=user)
+
+
+@app.route("/topic/<topic_id>/edit", methods=["GET", "POST"])
+def topic_edit(topic_id):
+    topic = db.query(Topic).get(int(topic_id))
+
+    if request.method == "GET":
+        return render_template("topic_edit.html", topic=topic)
+    elif request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+
+        # get current user (author)
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+
+        # check if user is logged in and user is author
+        if not user:
+            return redirect(url_for('login'))
+        elif topic.author.id != user.id:
+            return "You are not the author!"
+        else:
+            # update the topic fields
+            topic.title = title
+            topic.text = text
+            db.add(topic)
+            db.commit()
+
+            return redirect(url_for('topic_details', topic_id=topic_id))
+
+
+@app.route("/topic/<topic_id>/delete", methods=["GET", "POST"])
+def topic_delete(topic_id):
+    topic = db.query(Topic).get(int(topic_id))  # get topic from db by ID
+
+    if request.method == "GET":
+        return render_template("topic_delete.html", topic=topic)
+
+    elif request.method == "POST":
+        # get current user (author)
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+
+        # check if user is logged in and user is author
+        if not user:
+            return redirect(url_for('login'))
+        elif topic.author_id != user.id:
+            return "You are not the author!"
+        else:  # if user IS logged in and current user IS author
+            # delete topic
+            db.delete(topic)
+            db.commit()
+            return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
